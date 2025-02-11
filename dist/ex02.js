7ba57680442b903107a7c4641d6cb78e5b20b748
Tomoya <flyingfox193807@gmail.com>
2025-02-11 12:34:20 -0800

    finished

--- dist/ex02.js
+++ dist/ex02.js
@@ -1,18 +1,11 @@
+"use strict";
 // 2. Create a generic interface called `Data` with two properties: 
 // `id` (number) and `value` (of any type). 
 // Define an object of type `Data` where `value` is a string "Generic" and `id` is 1.
 // Log the object.
-
-interface Data<T, V> {
-  id: T
-  value: V
-}
-
-const data: Data<number, string> = { id: 1, value: "Generic" };
+const data = { id: 1, value: "Generic" };
 console.log(data); // Expected output: { id: 1, value: "Generic" }
-
-const swapElements = <T, U>(el1: T, el2: U): [U, T] => {
-    return [el2, el1]
-}
-
-console.log(swapElements(1, 'john'))
+const swapElements = (el1, el2) => {
+    return [el2, el1];
+};
+console.log(swapElements(1, 'john'));
--- dist/ex06.js
+++ dist/ex06.js
@@ -1,18 +1,15 @@
+"use strict";
 // 6. Create a generic arrow function called `getValueFromObject` that accepts an object and a key, 
 // and returns the value associated with that key from the object.
 // Use the object { js: "JavaScript", ts: "TypeScript" } and retrieve the value of the key "ts".
 // Log the result.
-
-const getValueFromObject = <T, U extends keyof T>(obj: T, key: U) => obj[key];
-
+const getValueFromObject = (obj, key) => obj[key];
 const languages = { js: "JavaScript", ts: "TypeScript" };
 console.log(getValueFromObject(languages, "ts")); // Expected output: "TypeScript"
-
-// const getPropValue = <T, U extends keyof T>(obj: T, key: U): T[U] => {
-//     return obj[key]
-//   }
-  
-//   console.log(getPropValue({
-//     name: "John",
-//     age: 50
-//   }, "age"))
+const setGoal = (title, description, date) => {
+    let courseGoal = {};
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal;
+};
+console.log(setGoal('WAD'));
